eb7d807b828f3aeac599fa8405f8799f5d63146e
Marco Ribeiro <mtiagotlr@gmail.com>
2025-04-14 00:29:08 +0100

    FEAT: Initial Structure

---
+++ src/car-auction-management/Presentation.API/Controllers/BaseController.cs
@@ -0,0 +1,9 @@
+﻿namespace Presentation.API.Controllers;
+
+using Microsoft.AspNetCore.Mvc;
+
+public class BaseController(ILogger<BaseController> logger) : ControllerBase
+{
+    protected const string DefaultRoute = "/v1/[controller]";
+    protected readonly ILogger<BaseController> Logger = logger;
+}
