c39ef8870d80cd6fc71ab7b8c52e05ce6f235d02
Marco Ribeiro <mtiagotlr@gmail.com>
2025-04-16 02:28:10 +0100

    Fix: fix the schema id for swagger

--- src/car-auction-management/Presentation.API/Program.cs
+++ src/car-auction-management/Presentation.API/Program.cs
@@ -30,7 +30,10 @@ public class Program
         });
         // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
         builder.Services.AddEndpointsApiExplorer();
-        builder.Services.AddSwaggerGen();
+        builder.Services.AddSwaggerGen((options) => 
+        {
+            options.CustomSchemaIds(type => type.FullName);
+        });
 
         builder.Services.AddSingleton<IVehicleFactory, VehicleFactory>();
         builder.Services.AddSingleton<IVehicleRepository, VehicleRepository>();
